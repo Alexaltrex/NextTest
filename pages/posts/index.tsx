21959112a2ad9fd7c6bd82defcf454e915945cee
AlexAltrex <alexaltrex@mail.ru>
2022-04-03 22:40:59 +0300

    add posts api

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -9,28 +9,28 @@ export interface IPost {
     text: string
 }
 
-// export const getStaticProps = async () => {
-//
-//     const response = await fetch(`https://next-test-ochre-three.vercel.app/api/posts`);
-//     const posts: IPost[] | null = await response.json();
-//     const subtitle = String(process.env.SUBTITLE) ?? "Default Subtitle";
-//     //console.log(process.env.SUBTITLE)
-//     if (!posts) {
-//         return {
-//             notFound: true,
-//         }
-//     } else {
-//         return {
-//             props: {
-//                 posts,
-//                 subtitle,
-//             }
-//         }
-//     }
-// }
+export const getStaticProps = async () => {
+
+    const response = await fetch(`https://next-test-ochre-three.vercel.app/api/post`);
+    const posts: IPost[] | null = await response.json();
+    const subtitle = String(process.env.SUBTITLE) ?? "Default Subtitle";
+    //console.log(process.env.SUBTITLE)
+    if (!posts) {
+        return {
+            notFound: true,
+        }
+    } else {
+        return {
+            props: {
+                posts,
+                subtitle,
+            }
+        }
+    }
+}
 
 const Posts = (
-    // {posts, subtitle}: {posts: IPost[], subtitle: string}
+    {posts, subtitle}: {posts: IPost[], subtitle: string}
     ) => {
     // console.log(subtitle)
     // console.log(posts)
@@ -39,20 +39,20 @@ const Posts = (
             <div>
                 <h1>Posts</h1>
                 <h2>
-                    subtitle
-                    {/*{subtitle}*/}
+                    {/*subtitle*/}
+                    {subtitle}
                 </h2>
                 <div className={style.posts}>
-                    posts
-                    {/*{*/}
-                    {/*    posts.map(post => (*/}
-                    {/*        <div key={post.id} className={style.post}>*/}
-                    {/*            /!*<p className={style.title}>{post.title}</p>*!/*/}
-                    {/*            <Link href={`/posts/${post.id}`}><a>{post.title}</a></Link>*/}
-                    {/*            /!*<p className={style.text}>{post.text}</p>*!/*/}
-                    {/*        </div>*/}
-                    {/*    ))*/}
-                    {/*}*/}
+                    {/*posts*/}
+                    {
+                        posts.map(post => (
+                            <div key={post.id} className={style.post}>
+                                {/*<p className={style.title}>{post.title}</p>*/}
+                                <Link href={`/posts/${post.id}`}><a>{post.title}</a></Link>
+                                {/*<p className={style.text}>{post.text}</p>*/}
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </MainLayout>
