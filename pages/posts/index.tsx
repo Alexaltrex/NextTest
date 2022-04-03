483f9390ef4f8a146fc4e64ee86bceb3aa9cf3c8
AlexAltrex <alexaltrex@mail.ru>
2022-04-03 23:33:24 +0300

    add posts api

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -13,8 +13,8 @@ export const getStaticProps = async () => {
     const response = await fetch(`https://next-test-ochre-three.vercel.app/api/post`);
     const responseJSON = await response.json();
     const posts = responseJSON.posts;
-    const subtitle = String(process.env.SUBTITLE) ?? "Default Subtitle";
-    //const subtitle = "Default Subtitle";
+    //const subtitle = String(process.env.SUBTITLE) ?? "Default Subtitle";
+    const subtitle = "Default Subtitle";
     //console.log(process.env.SUBTITLE)
     if (!posts) {
         return {
